9c64df506f525d6d823e8f9de4ec22f65d9228a3
Administrator <2039433571@qq.com>
2021-07-02 21:13:41 +0800

    Commit

---
+++ src/main/java/com/yallage/yamail/YaMail.java
@@ -0,0 +1,17 @@
+package com.yallage.yamail;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YaMail extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
