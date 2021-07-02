9461dd427c59e69f886fe63de0e25f57a5aa0368
Mengbill <2303784987@qq.com>
2021-07-02 21:50:26 +0800

    Commit

--- src/main/java/com/yallage/yamail/YaMail.java
+++ src/main/java/com/yallage/yamail/YaMail.java
@@ -1,17 +1,28 @@
 package com.yallage.yamail;
 
+import com.yallage.yamail.command.YaCommandExcutor;
+import com.yallage.yamail.listener.YaEventListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class YaMail extends JavaPlugin {
 
+    public static YaMail instance;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        saveDefaultConfig();
+        reloadConfig();
+        Bukkit.getLogger().info(getConfig().getString("prefix")+"> 正在加载YaMail插件......"+"版本"+getConfig().get("version"));
+        Bukkit.getPluginCommand("yamail").setExecutor(new YaCommandExcutor());
+        Bukkit.getPluginManager().registerEvents(new YaEventListener(),this);
+        Bukkit.getLogger().info(getConfig().getString("prefix")+"> 加载YaMail插件成功!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info(getConfig().getString("prefix")+"> YaMail插件已卸载 感谢您的使用");
     }
 }
---
+++ src/main/java/com/yallage/yamail/listener/YaEventListener.java
@@ -0,0 +1,7 @@
+package com.yallage.yamail.listener;
+
+import org.bukkit.event.Listener;
+
+public class YaEventListener implements Listener {
+
+}
