efc6be7b4f28d7d00a71d6cb5ba87d0d66ce8f77
Mengbill <2303784987@qq.com>
2021-07-03 18:50:09 +0800

    Commit

--- src/main/java/com/yallage/yamail/command/YaCommandExcutor.java
+++ src/main/java/com/yallage/yamail/command/YaCommandExcutor.java
@@ -5,12 +5,14 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.yallage.yamail.YaMail.instance;
 
@@ -59,11 +61,11 @@ public class YaCommandExcutor implements CommandExecutor {
                         Player player = (Player)sender;
                         Player target = Bukkit.getPlayer(args[1]);
                         if(target == null){
-                            player.sendMessage(instance.getConfig().getString("prefix")+"> §4§l该玩家离线或不存在 §2§l已转为离线邮件发送");
+                            player.sendMessage(instance.getConfig().getString("prefix")+"> §4§l该玩家离线或不存在 §a§l已转为离线邮件发送");
 
 
                         }else{
-                            player.sendMessage(instance.getConfig().getString("prefix")+"> §2§l该玩家当前在线 正在为您发送邮件");
+                            player.sendMessage(instance.getConfig().getString("prefix")+"> §a§l该玩家当前在线 正在为您发送邮件");
                             Inventory inventory = Bukkit.createInventory(null,54,"发送邮件");
                             ItemStack item_bk = new ItemStack(Material.PINK_STAINED_GLASS_PANE);
                             ItemMeta item_bk_meta = item_bk.getItemMeta();
@@ -73,6 +75,52 @@ public class YaCommandExcutor implements CommandExecutor {
                             for (int i : bk) {
                                 inventory.setItem(i,item_bk);
                             }
+                            ItemStack item_info1 = new ItemStack(Material.CLOCK);
+                            ItemMeta item_info1_meta = item_info1.getItemMeta();
+                            item_info1_meta.setDisplayName("§8§l收件人: §a§l"+target.getName());
+                            item_info1.setItemMeta(item_info1_meta);
+                            ItemStack item_info2 = new ItemStack(Material.COMPASS);
+                            ItemMeta item_info2_meta = item_info2.getItemMeta();
+                            item_info2_meta.setDisplayName("§8§l发件人: §a§l"+player.getName());
+                            List<String> lores = new ArrayList<String>();
+                            lores.add("● §8§l请将发送的物品放入上方空格处");
+                            lores.add("● §8§l如需发送犽币请点击左侧向日葵");
+                            lores.add("● §8§l如需发送点券请点击右侧下界之星");
+                            lores.add("● §8§l确认发送请点击绿色混凝土");
+                            lores.add("● §8§l取消发送请点击红色混凝土");
+                            item_info2_meta.setLore(lores);
+                            item_info2.setItemMeta(item_info2_meta);
+                            ItemStack item_button1 = new ItemStack(Material.GREEN_CONCRETE);
+                            ItemStack item_button2 = new ItemStack(Material.RED_CONCRETE);
+                            ItemMeta item_button1_meta = item_button1.getItemMeta();
+                            ItemMeta item_button2_meta = item_button2.getItemMeta();
+                            item_button1_meta.setDisplayName("§a§l确认");
+                            item_button2_meta.setDisplayName("§4§l取消");
+                            item_button1.setItemMeta(item_button1_meta);
+                            item_button2.setItemMeta(item_button2_meta);
+                            ItemStack item_button3 = new ItemStack(Material.SUNFLOWER);
+                            ItemStack item_button4 = new ItemStack(Material.NETHER_STAR);
+                            ItemMeta item_buitton3_meta = item_button3.getItemMeta();
+                            item_buitton3_meta.setDisplayName("§e§l犽币");
+                            List<String> button3_lores = new ArrayList();
+                            button3_lores.add("● §8§l左键增加100犽币");
+                            button3_lores.add("● §8§l右键减少100犽币");
+                            item_buitton3_meta.setLore(button3_lores);
+                            item_button3.setItemMeta(item_buitton3_meta);
+                            ItemMeta item_buitton4_meta = item_button4.getItemMeta();
+                            item_buitton4_meta.setDisplayName("§b§l点券");
+                            List<String> button4_lores = new ArrayList();
+                            button4_lores.add("● §8§l左键增加100点券");
+                            button4_lores.add("● §8§l右键减少100点券");
+                            item_buitton4_meta.setLore(button4_lores);
+                            item_button4.setItemMeta(item_buitton4_meta);
+                            inventory.setItem(4,item_info1);
+                            inventory.setItem(49,item_info2);
+                            inventory.setItem(47,item_button1);
+                            inventory.setItem(51,item_button2);
+                            inventory.setItem(45,item_button3);
+                            inventory.setItem(53,item_button4);
+                            player.openInventory(inventory);
                         }
                     }else{
                         sender.sendMessage(instance.getConfig().getString("prefix")+"> §4§l你输入的指令有误 /yamail help查看指令帮助");
